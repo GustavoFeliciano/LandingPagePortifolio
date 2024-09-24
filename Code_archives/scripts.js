5a3f749df69cf18be0e8651cefafadcdd04bf1a0
GustavoFeliciano <gustavo08_fonseca@outlook.com>
2024-09-24 16:45:29 -0300

    addin the first landing page

---
+++ Code_archives/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+document.querySelectorAll('.btn-agendar').forEach(button => {
+    button.addEventListener('click', () => {
+      alert("Funcionalidade de agendamento em breve!");
+    });
+  });
+  
